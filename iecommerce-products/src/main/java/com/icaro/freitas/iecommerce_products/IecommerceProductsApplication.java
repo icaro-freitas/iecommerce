2227806b0a542242b18a396cbadab87f192b5a07
icaro-freitas <icaro_freitas2014@outlook.com>
2025-04-28 17:30:19 -0300

    products service initial setup created

---
+++ iecommerce-products/src/main/java/com/icaro/freitas/iecommerce_products/IecommerceProductsApplication.java
@@ -0,0 +1,13 @@
+package com.icaro.freitas.iecommerce_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IecommerceProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IecommerceProductsApplication.class, args);
+	}
+
+}
