6275997a4fb5eadf6f62dbc7640e55fc13e0f6cd
icaro-freitas <icaro_freitas2014@outlook.com>
2025-04-29 15:55:19 -0300

    Product entity created

---
+++ iecommerce-products/src/main/java/com/icaro/freitas/iecommerce_products/entity/Product.java
@@ -0,0 +1,37 @@
+package com.icaro.freitas.iecommerce_products.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "products")
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	@Column(columnDefinition = "TEXT")
+	private String description;
+	@Column(precision = 10, scale = 2)
+	private BigDecimal price;
+	private Integer quantity;
+	private String imageUrl;
+	@Column(unique = true)
+	private String slug;
+    @Column(nullable = false)
+    private Boolean active = true;
+
+}
