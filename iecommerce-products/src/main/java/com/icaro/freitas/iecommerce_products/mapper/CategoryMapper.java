154811a67da6407e460a253611cb274932e68a71
icaro-freitas <icaro_freitas2014@outlook.com>
2025-05-27 19:30:56 -0300

    Category service and dto mapper created

---
+++ iecommerce-products/src/main/java/com/icaro/freitas/iecommerce_products/mapper/CategoryMapper.java
@@ -0,0 +1,15 @@
+package com.icaro.freitas.iecommerce_products.mapper;
+
+import com.icaro.freitas.iecommerce_products.dto.CategoryDto;
+import com.icaro.freitas.iecommerce_products.entity.Category;
+
+public class CategoryMapper {
+
+	public static CategoryDto toDto(Category category) {
+		CategoryDto categoryDto = new CategoryDto();
+		categoryDto.setId(category.getId());
+		categoryDto.setName(category.getName());
+		return categoryDto;
+	}
+	
+}
