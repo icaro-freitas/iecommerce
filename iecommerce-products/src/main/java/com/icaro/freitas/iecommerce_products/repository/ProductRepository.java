c0e9a7f82c78615a92983efde3399c3358b71e1c
icaro-freitas <icaro_freitas2014@outlook.com>
2025-06-02 21:38:29 -0300

    ProductDto and ProductService created

--- iecommerce-products/src/main/java/com/icaro/freitas/iecommerce_products/repository/ProductRepository.java
+++ iecommerce-products/src/main/java/com/icaro/freitas/iecommerce_products/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.icaro.freitas.iecommerce_products.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,7 @@ import com.icaro.freitas.iecommerce_products.entity.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
 	
+	@EntityGraph(attributePaths = "categories")
 	Page<Product> findAll(Pageable pageable);
 
 }
