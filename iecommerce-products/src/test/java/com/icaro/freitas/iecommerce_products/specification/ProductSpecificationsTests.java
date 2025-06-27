5480480de2d6fbe771e3cb4e8e8c56554eefd326
icaro-freitas <icaro_freitas2014@outlook.com>
2025-06-27 19:44:21 -0300

    ProductSpecificationsTests updated

--- iecommerce-products/src/test/java/com/icaro/freitas/iecommerce_products/specification/ProductSpecificationsTests.java
+++ iecommerce-products/src/test/java/com/icaro/freitas/iecommerce_products/specification/ProductSpecificationsTests.java
@@ -1,19 +1,21 @@
 package com.icaro.freitas.iecommerce_products.specification;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 
-import com.icaro.freitas.iecommerce_products.entity.Category;
 import com.icaro.freitas.iecommerce_products.entity.Product;
 import com.icaro.freitas.iecommerce_products.repository.ProductRepository;
-import com.icaro.freitas.iecommerce_products.testutil.ProductFactory;
 
 @DataJpaTest
 public class ProductSpecificationsTests {
@@ -21,82 +23,38 @@ public class ProductSpecificationsTests {
 	@Autowired
 	private ProductRepository repository;
 
-	private Product product;
-
-	@BeforeEach
-	private void setUp() throws Exception {
-		product = ProductFactory.createProduct();
+	@ParameterizedTest
+	@MethodSource("filterProvider")
+	void findAllshouldReturnExpectedProductWithGivenFilter(Specification<Product> spec, int expectedSize,
+			Long expectedId, String expectedName) {
+		assertFilteredResult(spec, expectedSize, expectedId, expectedName);
 	}
 
-	@Test
-	void nameContainsShouldReturnMatchingProduct() {
-		var spec = ProductSpecifications.nameContains("asus");
-		List<Product> result = repository.findAll(spec);
-		assertThat(result).containsOnly(product);
+	private static Stream<Arguments> filterProvider() {
+		return Stream.of(Arguments.of(ProductSpecifications.nameContains("asus"), 1, 1L, "Notebook asus vivobook"),
+				Arguments.of(ProductSpecifications.descriptionContains("ASUS"), 1, 1L, "Notebook asus vivobook"),
+				Arguments.of(ProductSpecifications.minPrice(new BigDecimal("5000.00")), 1, 5L, "Geladeira Philco"),
+				Arguments.of(ProductSpecifications.maxPrice(new BigDecimal("41.57")), 1, 8L,
+						"Livro Harry Potter e a Pedra Filosofal"),
+				Arguments.of(ProductSpecifications.minQuantity(195), 1, 3L, "Halter 4kg"),
+				Arguments.of(ProductSpecifications.maxQuantity(31), 1, 5L, "Geladeira Philco"),
+				Arguments.of(ProductSpecifications.isActive(true), 7, 1L, "Notebook asus vivobook"),
+				Arguments.of(ProductSpecifications.isActive(false), 1, 8L, "Livro Harry Potter e a Pedra Filosofal"),
+				Arguments.of(ProductSpecifications.categoryIdIn(List.of(3L)), 1, 3L, "Halter 4kg"));
 	}
 
-	@Test
-	void descriptionContainsShouldReturnMatchingProduct() {
-		var spec = ProductSpecifications.descriptionContains("ASUS");
-		List<Product> result = repository.findAll(spec);
-		assertThat(result).containsOnly(product);
-	}
+	private void assertFilteredResult(Specification<Product> spec, int expectedSize, Long expectedId,
+			String expectedName) {
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		Page<Product> result = repository.findAll(spec, pageRequest);
 
-	@Test
-	void categoryIdInShouldReturnProductInCategory() {
-		Long categoryId = 1L;
-		var spec = ProductSpecifications.categoryIdIn(List.of(categoryId));
-		List<Product> result = repository.findAll(spec);
-		assertThat(result)
-				.allSatisfy(p -> assertThat(p.getCategories()).extracting(Category::getId).contains(categoryId));
-	}
+		List<Product> productList = result.getContent();
+		Product firstProduct = productList.get(0);
 
-	@Test
-	void minPriceShouldReturnProductsWithGreaterOrEqualPrice() {
-		var testPrice = new BigDecimal("2000");
-		var spec = ProductSpecifications.minPrice(testPrice);
-		List<Product> result = repository.findAll(spec);
-		assertThat(result).allSatisfy(p -> assertThat(p.getPrice()).isGreaterThanOrEqualTo(testPrice));
-	}
-
-	@Test
-	void maxPriceShouldReturnProductsWithLessOrEqualPrice() {
-		var testPrice = new BigDecimal("3000");
-		var spec = ProductSpecifications.maxPrice(testPrice);
-		List<Product> result = repository.findAll(spec);
-		assertThat(result).allSatisfy(p -> assertThat(p.getPrice()).isLessThanOrEqualTo(testPrice));
-	}
-
-	@Test
-	void minQuantityShouldReturnProductsWithSufficientQuantity() {
-		var testQuantity = 20;
-		var spec = ProductSpecifications.minQuantity(testQuantity);
-		List<Product> result = repository.findAll(spec);
-		assertThat(result).allSatisfy(p -> assertThat(p.getQuantity()).isGreaterThanOrEqualTo(testQuantity));
-	}
-
-	@Test
-	void maxQuantityShouldReturnProductsWithLowQuantity() {
-		var testQuantity = 51;
-		var spec = ProductSpecifications.maxQuantity(testQuantity);
-		List<Product> result = repository.findAll(spec);
-		assertThat(result).allSatisfy(p -> assertThat(p.getQuantity()).isLessThanOrEqualTo(testQuantity));
-	}
-
-	@Test
-	void isActive_shouldReturnActiveProductsOnly() {
-		var activeValue = true;
-		var spec = ProductSpecifications.isActive(activeValue);
-		List<Product> result = repository.findAll(spec);
-		assertThat(result).allSatisfy(p -> assertThat(p.getActive()).isEqualTo(activeValue));
-	}
-
-	@Test
-	void isActiveShouldReturnInactiveProductsOnly() {
-		var inactiveValue = false;
-		var spec = ProductSpecifications.isActive(inactiveValue);
-		List<Product> result = repository.findAll(spec);
-		assertThat(result).allSatisfy(p -> assertThat(p.getActive()).isEqualTo(inactiveValue));
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(expectedSize, productList.size());
+		Assertions.assertEquals(expectedId, firstProduct.getId());
+		Assertions.assertEquals(expectedName, firstProduct.getName());
 	}
 
 }
